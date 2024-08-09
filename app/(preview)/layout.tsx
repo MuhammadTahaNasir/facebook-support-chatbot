99682e680cfb55e5d2a54487615606c79e92b6ed
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2024-08-10 00:45:14 +0500

    Create layout.tsx

---
+++ app/(preview)/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import { Metadata } from "next";
+import { Toaster } from "sonner";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://ai-sdk-preview-attachments.vercel.dev"),
+  title: "Facebook Support Chatbot",
+  description: "Get your queries resolved with Facebook Support Chatbot",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Toaster position="top-center" richColors />
+        {children}
+      </body>
+    </html>
+  );
+}
